d5ba5afcb4d448501dc5e6cae8a6b18ce767cd5c
Kristina <kristinashrestha2055@gmail.com>
2025-04-02 11:00:45 -0400

    adding getAll functiond

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   createProduct,
   getProductsByBusiness,
+  getAllProducts,
 } = require("../controllers/productController");
 const { protect, businessOnly } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/", protect, businessOnly, createProduct);
+router.get("/", getAllProducts);
 router.get("/:businessId", getProductsByBusiness);
 
 module.exports = router;
