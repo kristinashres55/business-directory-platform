478ee4207f31144f91f35c6a0f69dda3a7b38168
Kristina <kristinashrestha2055@gmail.com>
2025-04-02 08:51:46 -0400

    Registration and Login part

---
+++ backend/controllers/authController.js
@@ -0,0 +1,41 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
+
+exports.register = async (req, res) => {
+  const {
+    name,
+    email,
+    password,
+    role,
+    businessType,
+    description,
+    contactDetails,
+    location,
+  } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+    role,
+    businessType,
+    description,
+    contactDetails,
+    location,
+  });
+  res.status(201).json({ token: generateToken(user._id), user });
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  res.json({ token: generateToken(user._id), user });
+};
