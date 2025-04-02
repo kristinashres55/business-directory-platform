3282310c94e5654a92b7e7151120dc8f5035157d
Kristina <kristinashrestha2055@gmail.com>
2025-04-02 11:00:54 -0400

    adding getAll function

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -12,6 +12,15 @@ exports.createProduct = async (req, res) => {
   res.status(201).json(product);
 };
 
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find().populate("business", "name email");
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 exports.getProductsByBusiness = async (req, res) => {
   const products = await Product.find({ business: req.params.businessId });
   res.json(products);
